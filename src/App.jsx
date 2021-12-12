e9457c08bdb408ae5fd591a5d5db5edf8a77cffd
KenHirasawa <64624596+wakuwakusan1@users.noreply.github.com>
2021-12-12 19:54:24 +0900

    TodoDelete

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,29 @@ export const App = () => {
 
   const onClickAdd = () => {
     if (todoText === "") return; //入力されていないのに追加ボタンが押されたときの処理
+    //...は元の配列を傷つけずに配列のコピーをする。その後ろに新しいTodoを追加
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
+  };
+
+  ///削除ボタンが押された時の処理
+  //indexは何番目かを見るためにつける
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  //完了ボタンが押された時の処理
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1); //spliceで消す（更新）
+
+    //未完了のTodoのindex番目を完了のTodoの後ろにくっつけて新しい配列へ格納する
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
   };
 
   return (
@@ -34,15 +54,16 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTodo</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
+            //indexはTodoの何番目かを数えるもの
             return (
               //keyはDOMで使用する[差分]を比較する目印
               //mapなどでレンダリングしていくときにkeyを忘れずに書く
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
-              </div>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
+              </div> //onClickDeleteだけだと再レンダリングされたときに何回も関数を呼び出すからアロー関数で防ぐ
             );
           })}
         </ul>
